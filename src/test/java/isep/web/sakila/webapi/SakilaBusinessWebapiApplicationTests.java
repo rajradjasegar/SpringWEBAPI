f837269fc44f265194b2dbf78261deb8a89d7762
raj <radjasegar.raj@gmail.com>
2017-01-02 01:26:15 +0100

    first commit

---
+++ src/test/java/isep/web/sakila/webapi/SakilaBusinessWebapiApplicationTests.java
@@ -0,0 +1,18 @@
+package isep.web.sakila.webapi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = SakilaBusinessWebapiApplication.class)
+@WebAppConfiguration
+public class SakilaBusinessWebapiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
