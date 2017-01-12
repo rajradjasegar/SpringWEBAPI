2cef26a2f52467dff84dcd52dec495294bfdf769
raj <radjasegar.raj@gmail.com>
2017-01-12 01:19:24 +0100

    Connexion Static

---
+++ src/main/resources/static/js/jsLogin.js
@@ -0,0 +1,31 @@
+ $(document).ready(function () {
+    $('.forgot-pass').click(function(event) {
+      $(".pr-wrap").toggleClass("show-pass-reset");
+    }); 
+    
+    $('.pass-reset-submit').click(function(event) {
+      $(".pr-wrap").removeClass("show-pass-reset");
+    }); 
+});
+ 
+ 
+ var attempt = 3; 
+ 
+ function validate() {
+ 	var username = document.getElementById("username").value;
+ 	var password = document.getElementById("password").value;
+ 	if (username == "admin" && password == "admin") {
+ 		alert("Login successfully");
+ 		window.location = "home.html";
+ 		return false;
+ 	} else {
+ 		attempt--;
+ 		alert("You have left " + attempt + " attempt;");
+ 		if (attempt == 0) {
+ 			document.getElementById("username").disabled = true;
+ 			document.getElementById("password").disabled = true;
+ 			document.getElementById("submit").disabled = true;
+ 			return false;
+ 		}
+ 	}
+ }
